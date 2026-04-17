61b716e9fd74bc439004849f7daaf983e8d0416e
gregcarmo <gregorycarmo@gmail.com>
2026-04-17 00:37:00 -0300

    trying out links opening on new tab

--- src/components/statusbar/statusbar.component.js
+++ src/components/statusbar/statusbar.component.js
@@ -214,7 +214,7 @@ class Statusbar extends Component {
     this.refs.fastlink.onclick = () => {
       console.log(CONFIG.fastlink);
       if (CONFIG.config.fastlink) {
-        window.location.href = CONFIG.config.fastlink;
+        window.open(CONFIG.config.fastlink, "_blank", "noopener,noreferrer");
       }
     };
 
